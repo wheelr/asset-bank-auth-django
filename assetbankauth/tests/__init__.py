f9a1640f33fb53db3bf5eb71e6a1dafeb4c65ac6
kevinbennett <kevin@bright-interactive.co.uk>
2016-12-14 18:26:44 +0000

    Declare namespace in correct package

--- assetbankauth/tests/__init__.py
+++ assetbankauth/tests/__init__.py
@@ -1,5 +1,3 @@
 # -*- coding: utf-8 -*-
 # (c) 2016 Bright Interactive Limited. All rights reserved.
 # http://www.bright-interactive.com | info@bright-interactive.com
-
-__import__('pkg_resources').declare_namespace(__name__)
